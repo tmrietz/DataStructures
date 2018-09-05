74ae66eeee160022c73bb5f2e87fa2e44f0957f9
tmrietz <tmrietz@gmail.com>
2018-09-04 21:32:02 -0500

    make dynamic array template a header file

--- cpp/dynamicArray/dynamicArray.h
+++ cpp/dynamicArray/dynamicArray.h
@@ -1,24 +1,85 @@
 #ifndef DYNAMICARRAY_H
 #define DYNAMICARRAY_H
+#include <iostream>
+using namespace std;
 
+
+template <typename type>
 class dynamicArray{
     private:
         int* arr;
         int size, capacity;
 
     public:
-        dynamicArray();
-        dynamicArray(int cap);
-        ~dynamicArray();
-        
-        int getSize();
-        int getCapacity();
-        int getValueAt(int position);
-        void setValueAt(int position, int val);
-        void dynamicArrayInsert(int val);
-        void resizeArr();
-        bool isEmpty();
-        void printArr();
+        dynamicArray(){
+            arr = new type[10];
+            size = 0; capacity = 10;
+        }
+
+        dynamicArray(int cap){
+            arr = new type[cap];
+            size = 0; capacity = cap;
+        }
+
+        void pushBack(type val){
+            if(getSize() == getCapacity()){
+                resizeArr();
+            }
+            arr[getSize()] = val;
+            size++;
+        }
+
+        void removeBack(){
+            arr[getSize()] = NULL;
+            size--;
+        }
+
+        int getSize(){
+            return size;
+        }
+
+        int getCapacity(){
+            return capacity;
+        }
+
+        int getAt(int position){
+            return arr[position];
+        }
+
+        void setAt(int position, type val){
+            if(position >= 0 && position <= getSize()){
+                arr[position] = val;
+            } 
+        }
+
+        bool isEmpty(){
+            if(getSize() == 0){
+                return true;
+            }
+            return false;
+        }
+
+        void resizeArr(){
+            int* tmp = new type[capacity*2];
+            for(int s=0; s < getCapacity(); s++){
+                tmp[s] = arr[s];
+            }
+            delete [] arr;
+            arr = tmp;
+            capacity *= 2;
+        }
+
+        void printArr(){
+            for(int j=0; j < getSize(); j++){
+                cout << arr[j] << " ";
+            }
+            cout << endl;
+        }
+
+        ~dynamicArray(){
+            delete [] arr;
+            arr = nullptr;
+        }
 };
 
 #endif
